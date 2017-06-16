958fc7d69be6eb94c4c393fdec4fbd8aa23bf4e5
slaughtr <dslaughtr@gmail.com>
2017-06-16 11:20:04 -0700

    Update Settings layout styling, add save button

--- app/src/main/java/net/a40two/pext/ui/SettingsActivity.java
+++ app/src/main/java/net/a40two/pext/ui/SettingsActivity.java
@@ -1,51 +1,69 @@
 package net.a40two.pext.ui;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import net.a40two.pext.R;
 
 public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Spinner mExpireSpinner;
+    Spinner mPrivacySpinner;
+    Spinner mSyntaxSpinner;
+    Spinner mTextSizeSpinner;
+    Spinner mResultLimitSpinner;
+
+    //these will be used to save spinner positions to firebase/shared prefs
+    //so that they can later be used to load defaults
+    int expireSpinnerPos;
+    int privacySpinnerPos;
+    int syntaxSpinnerPos;
+
+    //and here we just want values, as there's no correlating
+    //spinners for these items
+    int textSizeSpinnerVal;
+    int resultLimitSpinnerVal;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         //set all the spinners
-        Spinner mExpireSpinner = (Spinner) this.findViewById(R.id.default_expiration_spinner);
+        mExpireSpinner = (Spinner) this.findViewById(R.id.default_expiration_spinner);
         ArrayAdapter<CharSequence> expireAdapter = ArrayAdapter.createFromResource(this,
                 R.array.expire_date_select, android.R.layout.simple_spinner_item);
         expireAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mExpireSpinner.setAdapter(expireAdapter);
 
-        Spinner mPrivacySpinner = (Spinner) this.findViewById(R.id.default_privacy_spinner);
+        mPrivacySpinner = (Spinner) this.findViewById(R.id.default_privacy_spinner);
         ArrayAdapter<CharSequence> privacyAdapter = ArrayAdapter.createFromResource(this,
                 R.array.private_select, android.R.layout.simple_spinner_item);
         privacyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mPrivacySpinner.setAdapter(privacyAdapter);
 
-        Spinner mSyntaxSpinner = (Spinner) this.findViewById(R.id.default_syntax_spinner);
+        mSyntaxSpinner = (Spinner) this.findViewById(R.id.default_syntax_spinner);
         ArrayAdapter<CharSequence> syntaxAdapter = ArrayAdapter.createFromResource(this,
                 R.array.syntax_highlight_select, android.R.layout.simple_spinner_item);
         syntaxAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSyntaxSpinner.setAdapter(syntaxAdapter);
 
-        Spinner mTextSizeSpinner = (Spinner) this.findViewById(R.id.text_size_spinner);
+        mTextSizeSpinner = (Spinner) this.findViewById(R.id.text_size_spinner);
         ArrayAdapter<CharSequence> textSizeAdapter = ArrayAdapter.createFromResource(this, R.array.editor_text_size_select, android.R.layout.simple_spinner_dropdown_item);
         mTextSizeSpinner.setAdapter(textSizeAdapter);
 
-        Spinner mResultLimitSpinner = (Spinner) this.findViewById(R.id.results_limit_spinner);
+        mResultLimitSpinner = (Spinner) this.findViewById(R.id.results_limit_spinner);
         ArrayAdapter<CharSequence> resultLimitAdapter = ArrayAdapter.createFromResource(this, R.array.result_limit_select, android.R.layout.simple_spinner_dropdown_item);
         mResultLimitSpinner.setAdapter(resultLimitAdapter);
 
         //set spinner click listeners
-        mSyntaxSpinner.setOnItemSelectedListener(this);
-        mPrivacySpinner.setOnItemSelectedListener(this);
         mExpireSpinner.setOnItemSelectedListener(this);
+        mPrivacySpinner.setOnItemSelectedListener(this);
+        mSyntaxSpinner.setOnItemSelectedListener(this);
         mTextSizeSpinner.setOnItemSelectedListener(this);
         mResultLimitSpinner.setOnItemSelectedListener(this);
 
@@ -54,7 +72,12 @@ public class SettingsActivity extends AppCompatActivity implements AdapterView.O
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+//        ((TextView) view).setTextColor(Color.RED);
+        if (view == mExpireSpinner) { }
+        if (view == mPrivacySpinner) { }
+        if (view == mSyntaxSpinner) { }
+        if (view == mTextSizeSpinner) { }
+        if (view == mResultLimitSpinner) { }
     }
 
     @Override
