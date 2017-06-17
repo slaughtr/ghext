60992fb9cacfc8b2f2955ceb887a5b2efb3c4706
slaughtr <dslaughtr@gmail.com>
2017-06-16 18:18:35 -0700

    Add test ad to help activity

--- app/src/main/java/net/a40two/pext/ui/HelpActivity.java
+++ app/src/main/java/net/a40two/pext/ui/HelpActivity.java
@@ -3,13 +3,26 @@ package net.a40two.pext.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import net.a40two.pext.R;
 
 public class HelpActivity extends AppCompatActivity {
+    //ads
+    private AdView mAdView;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        //initialize ads
+        MobileAds.initialize(getApplicationContext(),
+                "ca-app-pub-3940256099942544~3347511713");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
 }
